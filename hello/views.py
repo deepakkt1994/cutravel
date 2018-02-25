faa5984ebf8d5d48db380c6543eb1a323f5673e2
deepakkt1994 <deepakkt2008@gmail.com>
2018-02-25 11:29:00 -0700

    done

--- hello/views.py
+++ hello/views.py
@@ -4,12 +4,7 @@ import googlemaps
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-import cStringIO as StringIO
-from xhtml2pdf import pisa
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
-from cgi import escape
+
 
 from .models import Greeting
 
@@ -177,22 +172,3 @@ def joinPlan(request):
         return render(request, 'PlanSelection.html')
     else:
         return render(request, 'PlanSelection.html')
-def render_to_pdf(template_src, context_dict):
-    template = get_template(template_src)
-    context = Context(context_dict)
-    html  = template.render(context)
-    result = StringIO.StringIO()
-
-    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
-def html_to_pdf_view(request):
-    #Retrieve data or whatever you need
-    return render_to_pdf(
-            'pdf_template.html',
-            {
-                'pagesize':'A4',
-                'mylist': results,
-            }
-        )
