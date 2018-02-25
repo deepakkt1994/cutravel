dd0690d67002b325e18237627516662caa5a5518
Si Shen <shensilasg@gmail.com>
2018-02-25 09:21:10 -0700

    add google map embeded

--- hello/templates/views.py
+++ hello/templates/views.py
@@ -4,7 +4,40 @@ import googlemaps
 
 from .models import Greeting
 
-global_waypoints=[]
+
+import urllib.request
+import json
+
+class gmap_wrapper:
+    def __init__(self):
+        self.key_google_direction="AIzaSyD5QetA8YsrJ-jvQFd1hfRNLoNpVM9MHYY"
+        self.key_google_embeded="AIzaSyB2uz-9zoyW7GgkytCGnt636POGkTJyZuU"
+        self.key_google_geocoding="AIzaSyDzaIPhbF4dYeu_wvBlWPL40dqAia4Hfioself"
+    
+    #input: origin_str: str; dest_str: str, ways_point list of str, str should not contain " ", instead use "+"
+    #output: the order of ways_point (list of int, start from 0), not including origin_str/dest_str,
+    #         i.e. same size with ways_point
+    def get_direction_order(self, origin_str, dest_str, ways_point):
+        if(ways_point==None):
+            waypt=""
+        else:
+            waypt="|".join(ways_point)
+            waypt="optimize:true|"+waypt
+        url="https://maps.googleapis.com/maps/api/directions/json?"+ \
+           "origin="+ origin_str+ \
+           "&destination="+dest_str+ \
+           "&waypoints="+waypt+ \
+           "&key="+self.key_google_direction
+#        print(url)
+        res=urllib.request.urlopen(url)
+        data=json.load(res)
+#        print(data['routes'][0]['waypoint_order'])
+        return(data['routes'][0]['waypoint_order'])
+
+
+global_waystops=[]
+global_src=""
+global_dest=""
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -55,6 +88,7 @@ def startReq(request):
 
 def planSelect(request):
     print('RECEIVED REQUEST plan select: ', request.method)
+    global global_waystops, global_src, global_dest
     if request.method == 'POST':
         username = request.POST.get('name', '')
         print("Username: ", username)
@@ -85,11 +119,36 @@ def planSelect(request):
         distance = gmaps.distance_matrix(startpoint,endpoint)['rows'][0]['elements'][0]['distance']['value']
         #if(distance > 100000): add code to get mid point and calc nearby places again
         #context={'waystops':waystops}
-        global_waypoints=waystops 
-        return render(request, 'PlanSelection.html', { 'waystops':waystops})
+        #request.session['waystops']=waystops
+        global_waystops=waystops
+        global_src=startpoint.replace(' ','+')
+        global_dest=endpoint.replace(' ','+')
+        dct=dict()
+        dct['waystops']=waystops
+        dct['source']=global_src
+        dct['dest']=global_dest
+        return render(request, 'PlanSelection.html', dct)
     elif request.method == 'GET':
-        n_waypts=len(global_waypoints)
+        #waystops=request.session.get('waystops')
+        selected_waypoints=[]
+        n_waypts=len(global_waystops)
+        print(n_waypts)
         for i in range(n_waypts):
-            if(
+            varname="checkbox"+str(i)
+            if(request.GET.get(varname)):
+                selected_waypoints.append(global_waystops[i])
+        print(selected_waypoints)
+        waystops_name=[]
+        for wpt in selected_waypoints:
+            waystops_name.append(wpt["name"].replace(' ','+'))
+        print(waystops_name)
+        gm=gmap_wrapper()
+        order=gm.get_direction_order(global_src, global_dest, waystops_name)
+        print(order)
+        dct=dict()
+        dct['waystops']=waystops
+        dct['source']=global_src
+        dct['dest']=global_dest
+        return render(request, 'PlanSelection.html', dct)
     else:
         return render(request, 'StartPage.html')
