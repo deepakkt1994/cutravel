7001909650ea85786b767064691ad4cf0efa2822
deepakkt1994 <deepakkt2008@gmail.com>
2018-02-24 19:37:02 -0700

    get nearest places

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import googlemaps	
 
 from .models import Greeting
 
@@ -8,7 +9,28 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def getNearbyplaces(request):
+	api_key = 'AIzaSyD5QetA8YsrJ-jvQFd1hfRNLoNpVM9MHYY'
 
+	gmaps = googlemaps.Client(key=api_key)
+	geocode_result = gmaps.geocode('Boulder, CO') #$_GET['source']
+	latandlng=geocode_result[0]['geometry']['location']	
+	out = gmaps.places_nearby(latandlng,50)
+	places = out['results']
+	waystops=[]
+	for place in places:
+		waystops.append(place)
+	geocode_result = gmaps.geocode('Denver, CO') #$_GET['destination']
+	latandlng=geocode_result[0]['geometry']['location']	
+	out = gmaps.places_nearby(latandlng,50)
+	places = out['results']
+	for place in places:
+		waystops.append(place)
+	distance = gmaps.distance_matrix('Boulder, CO','Denver, CO')['rows'][0]['elements'][0]['distance']['value']
+	#if(distance > 100000): add code to get mid point and calc nearby places again
+	
+	print waystops
+	return waystops
 def db(request):
 
     greeting = Greeting()
