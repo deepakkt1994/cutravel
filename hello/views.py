d65f934614c110382ade4e144c5654eead123266
Si Shen <shensilasg@gmail.com>
2018-02-25 02:02:47 -0700

    test add_places, google_map_show

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,7 @@ import googlemaps
 
 from .models import Greeting
 
+global_waystops=[]
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -84,7 +85,12 @@ def planSelect(request):
         distance = gmaps.distance_matrix(startpoint,endpoint)['rows'][0]['elements'][0]['distance']['value']
         #if(distance > 100000): add code to get mid point and calc nearby places again
         #context={'waystops':waystops}
-       
+        global_waystops=waystops 
         return render(request, 'PlanSelection.html', { 'waystops':waystops})
     else:
         return render(request, 'StartPage.html')
+
+#def addPlaces(request):
+#    print('RECEIVED REQUEST add places: ', request.method)
+#    if request.method == "POST":
+
